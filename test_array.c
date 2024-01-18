0f359598445e552f5dbf9b997c5650b0ccb4bba2
Bing Wen Tan <btan@c2r2s11.42singapore.sg>
2024-01-18 19:06:21 +0800

    modified:   test_array.c

--- test_array.c
+++ test_array.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:22:59 by btan              #+#    #+#             */
-/*   Updated: 2024/01/18 18:07:53 by btan             ###   ########.fr       */
+/*   Updated: 2024/01/18 19:02:27 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	swap(int *lst)
 
 void	push(int *lst_1, int *lst_2)
 {
-	ft_swap(&lst_1[0], &lst_2[0])
+	ft_swap(&lst_1[0], &lst_2[0]);
 }
 
 int	main()
@@ -31,6 +31,15 @@ int	main()
 	stack_b = ft_calloc(3, sizeof(int *));
 	stack_a[0] = 5;
 	stack_a[1] = 1;
+	stack_b[0] = 5;
+	stack_b[1] = 1;
+	ft_printf("stack_a: {%d, %d}\n", stack_a[0], stack_a[1]);
+	ft_printf("stack_b: {%d, %d}\n", stack_b[0], stack_b[1]);
 	swap(stack_a);
-	ft_printf("%d\n", stack_a[0]);
+	swap(stack_b);
+	ft_printf("stack_a: {%d, %d}\n", stack_a[0], stack_a[1]);
+	ft_printf("stack_b: {%d, %d}\n", stack_b[0], stack_b[1]);
+	push(stack_a, stack_b);
+	ft_printf("stack_a: {%d, %d}\n", stack_a[0], stack_a[1]);
+	ft_printf("stack_b: {%d, %d}\n", stack_b[0], stack_b[1]);
 }	
