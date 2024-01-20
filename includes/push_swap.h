22f736cbe82783f6ed5d115d5c7a81c7ed50cdc4
Bing Wen Tan <btan@student.42singapore.sg>
2024-01-20 15:03:00 +0800

    Added swap, push, rotate and reverse rotate prototypes to header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:51:25 by btan              #+#    #+#             */
-/*   Updated: 2024/01/20 14:54:50 by btan             ###   ########.fr       */
+/*   Updated: 2024/01/20 15:02:26 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,20 @@ void	swap(t_list **head);
 void	push(t_list **head_a, t_list **head_b);
 void	rotate(t_list **head);
 void	reverse_rotate(t_list **head);
+
+void	sa(t_list **head_a);
+void	sb(t_list **head_b);
+void	ss(t_list **head_a, t_list **head_b);
+
+void	pa(t_list **head_a, t_list **head_b);
+void	pb(t_list **head_a, t_list **head_b);
+
+void	ra(t_list **head_a);
+void	rb(t_list **head_b);
+void	rr(t_list **head_a, t_list **head_b);
+
+void	rra(t_list **head_a);
+void	rrb(t_list **head_b);
+void	rrr(t_list **head_a, t_list **head_b);
 
 #endif
