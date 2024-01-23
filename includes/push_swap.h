f3d5b1bd531a3e839c8131fa2c6a9c5117bb2ff0
Bing Wen Tan <btan@c2r5s10.42singapore.sg>
2024-01-23 18:21:31 +0800

    Added ft_atol()

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 14:51:25 by btan              #+#    #+#             */
-/*   Updated: 2024/01/23 13:40:45 by btan             ###   ########.fr       */
+/*   Updated: 2024/01/23 18:21:15 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct s_content
 	int	rank;
 }	t_content;
 
+long	ft_atol(const char *str);
 int		ft_strslen(char **strs);
 int		*strs_to_ints(char **strs);
 
--- srcs/push_swap_utils.c
+++ srcs/push_swap_utils.c
@@ -6,11 +6,38 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 13:26:08 by btan              #+#    #+#             */
-/*   Updated: 2024/01/23 03:16:38 by btan             ###   ########.fr       */
+/*   Updated: 2024/01/23 18:21:05 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+long	ft_atol(const char *str)
+{
+	long	i;
+	long	value;
+	long	neg;
+
+	i = 0;
+	value = 0;
+	neg = 1;
+	while ((str[i] > 8 && str[i] < 14) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg = -neg;
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (value * neg);
+		value = value * 10 + str[i] - '0';
+		i++;
+	}
+	return (value * neg);
+}
 
 int	ft_strslen(char **strs)
 {
@@ -40,3 +67,4 @@ int	*strs_to_ints(char **strs)
 	ints -= ft_strslen(strs);
 	return (ints);
 }
+
