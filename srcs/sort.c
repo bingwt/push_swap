d6450886c9f615b7c350491c02c2af569381d0f7
Bing Wen Tan <btan@student.42singapore.sg>
2024-01-30 13:57:10 +0800

    Added chunk_sort()

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 21:24:53 by btan              #+#    #+#             */
-/*   Updated: 2024/01/26 14:17:57 by btan             ###   ########.fr       */
+/*   Updated: 2024/01/30 13:32:22 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,12 +128,164 @@ void	split_stack(t_list **head_a, t_list **head_b)
 	}
 }
 
+// void	sort(t_list **head_a, t_list **head_b)
+// {
+// 	split_stack(head_a, head_b);
+// 	// simple_sort(head_a, head_b);
+// 	// while (ft_lstsize(*head_a) > 3)
+// 	// {
+// 	// 	pb(head_a, head_b);
+// 	// }
+// }
+
+void	rotate_to_topa(t_list **head, int rank)
+{
+	t_list		*current;
+	int			max;
+	int			mid;
+	int			midpoint;
+
+	current = *head;
+	max = stack_max(*head);
+	mid = stack_mid(*head);
+	midpoint = 0;
+	while (current)
+	{
+		if (((t_content *)current->content)->rank == mid && !midpoint)
+			midpoint++;
+		if (((t_content *)current->content)->rank == max && !midpoint)
+			ra(head);
+		if (((t_content *)current->content)->rank == max && midpoint)
+			rra(head);
+		if (((t_content *)(*head)->content)->rank == rank)
+			break ;
+		current = current->next;
+	}
+}
+
+void	rotate_to_topb(t_list **head, int rank)
+{
+	t_list		*current;
+	int			max;
+	int			mid;
+	int			midpoint;
+
+	current = *head;
+	max = stack_max(*head);
+	mid = stack_mid(*head);
+	midpoint = 0;
+	while (current)
+	{
+		if (((t_content *)current->content)->rank == mid && !midpoint)
+			midpoint++;
+		if (((t_content *)current->content)->rank == max && !midpoint)
+			rb(head);
+		if (((t_content *)current->content)->rank == max && midpoint)
+			rrb(head);
+		if (((t_content *)(*head)->content)->rank == rank)
+			break ;
+		current = current->next;
+	}
+}
+
+void	cheapest_to_topa(t_list **head)
+{
+	init_cost(head);
+	print_cost(*head, NULL);
+}
+
+void	chunk_sort(t_list **head_a, t_list **head_b)
+{
+	int	chunk_size;
+	int	chunks;
+	int	remainder;
+	int	current_chunk;
+	int	len;
+
+	chunk_size = 10;
+	if (ft_lstsize(*head_a) < 12)
+		chunk_size = 3;
+	chunks = ft_lstsize(*head_a) / chunk_size;	
+	remainder = ft_lstsize(*head_a) % chunk_size;
+	if (remainder)
+		chunks++;
+	current_chunk = chunk_size;
+	while (current_chunk <= (chunks * 10))
+	{
+		len = ft_lstsize(*head_a);
+		while (len--)
+		{
+			if (!ft_lstsize(*head_b))
+				pb(head_a, head_b);
+			if (((t_content *)(*head_a)->content)->rank <= current_chunk)
+				pb(head_a, head_b);
+			else
+				ra(head_a);
+			// if (((current_chunk / chunk_size) % 2) && current_chunk > chunk_size)
+			// 		rb(head_b);
+		}
+		current_chunk += chunk_size;
+	}
+	len = ft_lstsize(*head_b);
+	init_pos(head_b);
+	while (len--)
+	{
+		while (((t_content *)(*head_b)->content)->rank != stack_max(*head_b))
+			rotate_to_topb(head_b, stack_max(*head_b));
+		pa(head_b, head_a);
+	}
+}
+
+int	max_bits(t_list *head)
+{
+	int	max;
+	int	bits;
+
+	max = stack_max(head);
+	bits = 0;
+	while (max)
+	{
+		max >>= 1;
+		bits++;
+	}
+	return (bits);
+}
+
+int check_bit(int num, int pos)
+{
+    int mask;
+	
+	mask = 1 << (pos - 1);
+    return (num & mask) != 0;
+}
+
+void	radix_sort(t_list **head_a, t_list **head_b)
+{
+    int	max_bit;
+    int	len;
+
+    max_bit = 2;
+    while (max_bit--)
+    {
+		len = ft_lstsize(*head_a);
+        while (len--)
+		{
+			if (check_bit(((t_content *)(*head_a)->content)->rank, max_bit + 1))
+				pb(head_a, head_b);
+			else
+				ra(head_a);
+		}
+		// while (ft_lstsize(*head_b))
+		// 	pa(head_b, head_a);
+    }
+}
+
 void	sort(t_list **head_a, t_list **head_b)
 {
-	split_stack(head_a, head_b);
-	// simple_sort(head_a, head_b);
-	// while (ft_lstsize(*head_a) > 3)
-	// {
-	// 	pb(head_a, head_b);
-	// }
+	// if (ft_lstsize(*head_a) <= 100 )
+	// 	chunk_sort(head_a, head_b);
+	// else
+		// radix_sort(head_a, head_b);
+		// chunk_sort(head_a, head_b);
+		cheapest_to_topa(head_a);
 }
